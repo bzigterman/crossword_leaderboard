62882fb725fd6d74798a415e4142bd7491778c06
Ben Zigterman <ben@bzigterman.com>
2023-03-18 21:20:31 -0500

    switch back to mac

--- install-packages.R
+++ install-packages.R
@@ -3,6 +3,6 @@ options(install.packages.check.source = "no")
 install.packages("tidyverse")
 install.packages("slackr")
 
-# update.packages(ask = FALSE,
-#                 checkBuilt = TRUE)
+update.packages(ask = FALSE,
+                checkBuilt = TRUE)
 
