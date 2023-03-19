ed908298a44bba90b6df91e8b34f92235d383dd4
Ben Zigterman <ben@bzigterman.com>
2023-03-19 12:13:36 -0500

    hopefully fix error

--- poster.R
+++ poster.R
@@ -3,23 +3,6 @@ library(tidyverse)
 library(slackr)
 library(googlesheets4)
 
-token <- paste0("{
-  \"type\": \"service_account\",
-  \"project_id\": \"",Sys.getenv("PROJECT_ID"),"\",
-  \"private_key_id\": \"",Sys.getenv("PRIVATE_KEY_ID"),"\",
-  \"private_key\": \"",Sys.getenv("PRIVATE_KEY"),"\",
-  \"client_email\": \"",Sys.getenv("CLIENT_EMAIL"),"\",
-  \"client_id\": \"",Sys.getenv("CLIENT_ID"),"\",
-  \"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\",
-  \"token_uri\": \"https://oauth2.googleapis.com/token\",
-  \"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\",
-  \"client_x509_cert_url\": \"",Sys.getenv("CLIENT_X509_CERT_URL"),"\"
-}
-")
-
-write(x = token,
-      file = "token.json",
-      sep = "")
 gs4_auth(path = "token.json",
          email = Sys.getenv("GOOGLE_EMAIL"))
 
