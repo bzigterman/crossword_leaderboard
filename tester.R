9d1d0f704b5163b1f1592cc1cde77c9b008742d7
Ben Zigterman <ben@bzigterman.com>
2023-03-20 21:00:42 -0500

    create tester

---
+++ tester.R
@@ -0,0 +1,49 @@
+library(httr)
+library(tidyverse)
+library(slackr)
+library(googlesheets4)
+library(gargle)
+library(sodium)
+library(googledrive)
+
+# Google API ----
+json <- Sys.getenv("TOKEN_KEY") |> 
+  stringr::str_replace_all(pattern = fixed("\\n"),
+                           replacement = "\n")
+dec <- rawToChar( jsonlite::base64_dec( json))
+
+gs4_auth(path = dec)
+
+# read data ----
+
+old_csv <- read_sheet(ss = Sys.getenv("SHEET_ID"),
+                      sheet = "Sheet1",
+                      col_types = "ccD")
+
+today <- force_tz(today(tzone = "America/Chicago"),tzone = "America/Chicago")
+
+final_results <- old_csv |> 
+  filter(date == today)
+final_results_date <- force_tz(as_date( final_results$date[[1]],
+                                        tz = "America/Chicago"),
+                               tzone = "America/Chicago")
+final_results_date_text <- strftime(x = final_results_date, 
+                                    tz = "US/Central",
+                                    format = "%A, %B %d")
+
+nyt_leaderboard_text1 <- final_results |> 
+  select(name,time) |> 
+  mutate(nametime = paste0(name,": ",time,"\n")) |> 
+  select(nametime)
+Results <- paste0(final_results_date_text,
+                  "\n",
+                  paste0(nyt_leaderboard_text1$nametime, 
+                         collapse = ""))
+Results
+
+# post data ----
+
+if (final_results_date == today) {
+  slackr_bot(Results,
+             incoming_webhook_url = Sys.getenv("SLACK_TEST_URL"))
+}
