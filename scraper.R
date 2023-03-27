9771f01067c6c7a662f373cec3a6e7a6a436cc84
Ben Zigterman <ben@bzigterman.com>
2023-03-26 20:17:50 -0500

    try to fix error with ties

--- scraper.R
+++ scraper.R
@@ -33,21 +33,43 @@ nyt_crossword_date_text <- strftime(x = nyt_crossword_date,
 
 today <- today(tzone = "America/Chicago")
 
-nyt_leaderboard <- read_html(nyt) |> 
+names <- read_html(nyt) |> 
   html_elements(".lbd-board__items") |> 
-  html_elements(".lbd-score") |> 
+  html_elements(".lbd-score__name") |> 
   html_text2() |> 
   as_tibble() |> 
-  separate_wider_delim(cols = value, delim = "\n", names = c("rank",
-                                                             "blank",
-                                                             "name",
-                                                             "blank2","time")) |> 
-  select(rank,name,time) |> 
+  mutate(name = value) |> 
+  select(!value)
+times <- read_html(nyt) |> 
+  html_elements(".lbd-board__items") |> 
+  html_elements(".lbd-score__time") |> 
+  html_text2() |> 
+  as_tibble() |> 
+  mutate(time = value) |> 
+  select(!value)
+
+nyt_leaderboard <- cbind(names,times) |> 
   mutate(name = if_else(name == "Ben (you)","Ben",name)) |> 
   mutate(date = as_date( nyt_crossword_date, tz = "America/Chicago")) |> 
   mutate(time = if_else(time == "Play Puzzle","--",time)) |> 
-  filter(time != "--") |> 
-  select(!rank)
+  filter(time != "--")
+
+
+# nyt_leaderboard <- read_html(nyt) |> 
+#   html_elements(".lbd-board__items") |> 
+#   html_elements(".lbd-score__name") |> 
+#   html_text2() |> 
+#   as_tibble() #|> 
+#   separate_wider_delim(cols = value, delim = "\n", names = c("rank",
+#                                                              "blank",
+#                                                              "name",
+#                                                              "blank2","time")) |> 
+#   select(rank,name,time) |> 
+#   mutate(name = if_else(name == "Ben (you)","Ben",name)) |> 
+#   mutate(date = as_date( nyt_crossword_date, tz = "America/Chicago")) |> 
+#   mutate(time = if_else(time == "Play Puzzle","--",time)) |> 
+#   filter(time != "--") |> 
+#   select(!rank)
 
 nyt_leaderboard
 nyt_leaderboard_text1 <- nyt_leaderboard |> 
