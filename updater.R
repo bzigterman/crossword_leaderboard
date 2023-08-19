ea0dfe94374a3551528f0f22116254f81da14340
Ben Zigterman <ben@bzigterman.com>
2023-08-19 16:32:30 -0500

    remove duplicate entries

--- updater.R
+++ updater.R
@@ -66,7 +66,9 @@ old_csv <- read_sheet(ss = Sys.getenv("SHEET_ID"),
                       col_types = "TccD") |> 
   mutate(time = if_else(substr(time,0,1) == ":",
                         paste0("0",time),
-                        time)) 
+                        time))  |> 
+  select(name,time,date) |> 
+  distinct()
 
 old_csv_today <- old_csv |> 
   filter(date == nyt_crossword_date)
