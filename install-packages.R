d777bb3cd4a88201e4ba05c419422cf8e7472197
Ben Zigterman <ben@bzigterman.com>
2025-02-12 12:00:33 -0600

    use github version of slackr

--- install-packages.R
+++ install-packages.R
@@ -1,11 +1,14 @@
 options(install.packages.check.source = "no")
 
 install.packages("tidyverse")
-install.packages("slackr")
+#install.packages("slackr")
 install.packages("jsonlite")
 install.packages('ggbeeswarm')
 install.packages("jsonlite")
 install.packages("janitor")
+
+install.packages("remotes")
+remotes::install_github("mrkaye97/slackr")
 
 update.packages(ask = FALSE,
                 checkBuilt = TRUE)
